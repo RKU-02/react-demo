a2b093ce9af244d16e685252d97795525482d301
RKU-02 <rajubhadiya999@gmail.com>
2023-03-02 10:33:25 +0530

    DAY-6 task-2 of array.map funcrion is complete.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-
-import './App.css';
-import React, { useState } from 'react';
-import Home from './pages/Home';
-import About from './pages/About';
-import Title from './component/Title'
+import "./App.css";
+import React, { useState } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Title from "./component/Title";
 
 export default function App() {
 
-  const[text,setText]=useState(true)
-
-  const change=()=>{
-    setText(!text)
-  }
   
+  // const [text, setText] = useState(true);
+// 
+  // const change = () => {
+    // setText(!text);
+  // };
+
   return (
     <div className="app">
-     {text ? <Home/> : <About/>} 
-    <button className='btn' onClick={change}>Clik</button>
+      {/* {text ? <Home/> : <About/>}  */}
+
+      {/* <button className='btn' onClick={change}>Clik</button> */}
+
+      <Title />
     </div>
   );
 }
--- src/component/Title.js
+++ src/component/Title.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 
-export default class Title extends Component {
-  render() {
-    return (
-      <div>
-        <h2 style={{color:'aqua'}}>hello Tatvasoft</h2>
-      </div>
-    )
-  }
+export default function Title() {
+  const data = [
+    {
+      index: 1,
+      name: "ABC",
+      work: "designer",
+    },
+    {
+      index: 2,
+      name: "XYZ",
+      work: "haker",
+    },
+    {
+      index: 3,
+      name: "MNP",
+      work: "dewloper",
+    },
+  ];
+  return (
+    <div>
+      {data.map((user) => {
+        return (
+          <div className="data" key={user.index}>
+            <h2>
+              {user.index}: I am {user.name} And I am {user.work}.
+            </h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
